cb7416601256f5f69ef9c1be52514e288bbb22c5
shuaib shaban <shuaib>
2022-12-07 11:50:14 +0300

    mini-project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+// your code here
+
+  document.addEventListener("DOMContentLoaded", () => {
+    const form = document.querySelector("#create-task-form");
+  
+  
+    const renderTodoItem = function (e) {
+      e.preventDefault();
+      const todoItemInput = document.querySelector("#new-task-description").value;
+      const todoList = document.querySelector("#tasks");
+      const item = document.createElement("li");
+      item.textContent = todoItemInput;
+  
+      todoList.prepend(item);
+  
+  
+      const deleteBtn = document.createElement("button");
+      deleteBtn.innerHTML = "Delete";
+      item.append(deleteBtn);
+      const delTodo = function () {
+        item.remove()
+      };
+      deleteBtn.addEventListener("click", delTodo);
+    };
+  
+    form.addEventListener("submit", renderTodoItem);
+  });
